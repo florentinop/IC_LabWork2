ec9aedeccc41cc8c4b7ec858f70e5bc6187189d6
rt <florentino@ua.pt>
2022-12-01 19:34:34 +0000

    README

--- part2/ImageCodec.cpp
+++ part2/ImageCodec.cpp
@@ -16,15 +16,20 @@ int main(int argc, char* argv[]) {
         cerr << "                        Default: -d\n";
         return 1;
     }
+    
+    
     int mode = 0;
     if(string(argv[1]) == "-e")
         mode = 1;
     // decodes
     if (mode == 0) {
+        const clock_t begin = clock();
         ImageCodec codec{string(argv[argc - 2])};
         codec.decode(string(argv[argc - 1]));
+        cout << "Decoding time: " << float(clock() - begin) / CLOCKS_PER_SEC << '\n';
     // encodes
     } else {
+        const clock_t begin = clock();
         Mat img = imread(string(argv[argc - 2]), IMREAD_COLOR);
         if (img.empty()) {
             cout << "Could not read the image: " << argv[argc - 2] << endl;
@@ -32,6 +37,7 @@ int main(int argc, char* argv[]) {
         }
         ImageCodec codec{string(argv[argc - 1])};
         codec.encode(img);
+        cout << "Encoding time: " << float(clock() - begin) / CLOCKS_PER_SEC << '\n';
     }
     return 0;
 }
